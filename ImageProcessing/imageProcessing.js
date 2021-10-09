5f5dbf1e93bf4858beae52961531c50f8b689f54
mw097 <mihelone@gmail.com>
2021-10-09 19:21:24 +0200

    Energy Map calculation refactor

--- ImageProcessing/imageProcessing.js
+++ ImageProcessing/imageProcessing.js
@@ -2,6 +2,6 @@ import {getImageDataFromFile, getImageDataFromImageData} from './imageLoad.js';
 import {getEnergyMap} from './seamCarving.js';
 
 getImageDataFromFile('./sample.jpeg').then(imageData => {
-    let processedImageData = getEnergyMap(imageData);
-    getImageDataFromImageData(processedImageData);
+    getEnergyMap(imageData, 'red');
+    // getImageDataFromImageData(processedImageData);
 });
--- ImageProcessing/seamCarving.js
+++ ImageProcessing/seamCarving.js
@@ -1,63 +1,78 @@
-const STEP_RGBA = 4;
-const RGBAIndexes = {
-    red: 0,
-    green: 1,
-    blue: 2,
-    alpha: 3,
+const resizeImageWidth = (image, width) => {
+    const ImageSize = calculateImageSize(image);
+    const pxToRemove = image.width - width;
+    if ( pxToRemove < 0 ) {
+        throw new Error('Upsizing no supported.');
+    }
+    
+    let energyMap = [];
+    let seam = [];
+
+    for (let i = 0; i < pxToRemove; i+=1) {
+        energyMap = calculateEnergyMap(image, ImageSize);
+        seam = findLowEnergySeam(image, ImageSize);
+        deleteSeam(seam, image, ImageSize);
+
+        ImageSize.width -= 1;
+    }
+
+    return {image, ImageSize}
 };
 
-const getChannelMatrixMap = async (imageData, channel) => {
-    if (!(channel in RGBAIndexes)) return Error('Wrong channel');
-    
-    const {data, width: columns} = imageData;
-    let channelMatrixMap = [];
-    let columnArr = [];
+const getPixelEnergy = (leftPixel, middlePixel, rightPixel) => {
+    const [mR, mG, mB] = middlePixel;
 
-    for (let i = RGBAIndexes[channel]; i < data.length; i += STEP_RGBA) {
-        columnArr.push(data[i]);
-        if (columnArr.length === columns) {
-            channelMatrixMap.push(columnArr);
-            columnArr = [];
+    let lEnergy = 0;
+    if (leftPixel) {
+        const [lR, lG, lB] = leftPixel;
+        lEnergy = (lR-mR) ** 2 + (lG-mG) ** 2 + (lB-mB) ** 2;
+    }
+
+    let rEnergy = 0;
+    if (rightPixel) {
+        const [rR, rG, rB] = rightPixel;
+        lEnergy = (rR-rR) ** 2 + (rG-rG) ** 2 + (rB-rB) ** 2;
+    }
+
+    return Math.sqrt(lEnergy + rEnergy);
+};
+
+const getPixel = (image, coordinate) => {
+    const RGBACellSize = 4;
+    const index = coordinate.y * image.width + coordinate.x;
+
+    return image.data.subarray( index * RGBACellSize, index * RGBACellSize + RGBACellSize );
+};
+
+const setPixel = (image, coordinate, color) => {
+    const RGBACellSize = 4;
+    const index = coordinate.y * image.width + coordinate.x;
+
+    image.data.set(color, index * RGBACellSize);
+};
+
+const createMatrix = (width, height, value) => {
+    return new Array(height)
+        .fill(null)
+        .map(() => {
+            return new Array(width)
+                .fill(value);
+        });
+};
+
+const calculateEnergyMap = (image, imageSize) => {
+    const {width, height} = imageSize;
+
+    const energyMap = createMatrix(width, height, Infinity);
+    for (let y = 0; y < height; y += 1) {
+        for (let x = 0; x < width; x += 1) {
+            const leftPixel = (x-1) >= 0 ? getPixel(image, {x: x-1, y}) : null;
+            const middlePixel = getPixel(image, {x, y});
+            const rigthPixel = (x+1) < w ? getPixel(image, {x: x+1, y}) : null;
+
+            energyMap[y][x] = getPixelEnergy(leftPixel, middlePixel, rigthPixel);
         }
     }
 
-    return channelMatrixMap;
-};
-
-const getChannelMap = async (imageData, channelSettings) => {
-    const {red, green, blue, alpha} = channelSettings;
-    
-    imageData.data.forEach((value, index, data) => {
-        data[STEP_RGBA*index + RGBAIndexes.red] = red ?? data[STEP_RGBA*index + RGBAIndexes.red];
-        data[STEP_RGBA*index + RGBAIndexes.green] = green ?? data[STEP_RGBA*index + RGBAIndexes.green];
-        data[STEP_RGBA*index + RGBAIndexes.blue] = blue ?? data[STEP_RGBA*index + RGBAIndexes.blue];
-        data[STEP_RGBA*index + RGBAIndexes.alpha] = alpha ?? data[STEP_RGBA*index + RGBAIndexes.alpha];
-    });
-
-    return imageData;
-};
-
-const getChannelEnergyMap = async (imageData, channel) => {
-    // let channelSettings = {
-    //     red: 0,
-    //     green: 0,
-    //     blue: 0,
-    //     alpha: 0
-    // };
-    // let redChannelMap = await getChannelMatrixMap(imageData, channelSettings);
-
-
     return energyMap;
 };
-
-const getEnergyMap = async (imageData, channel) => {
-    let channelSettings = {
-        // red: 0,
-        // green: 0,
-        // blue: 0,
-        // alpha: 0
-    };
-    return await getChannelMap(imageData, channelSettings);
-};
-
-export {getEnergyMap};
